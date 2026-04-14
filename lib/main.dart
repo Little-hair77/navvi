530319c379bddfa7f36e8a0798074362c01e24fa
Pablo Henrique <pablo.henriquecba1@gmail.com>
2026-04-14 20:03:59 -0300

    add main

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:navvi/home_screen.dart';
+
+import 'home_screen.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: NavviApp(),
+  ));
+}
